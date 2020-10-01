9a621b4829a1326362834f45c5d3411cf0a8407e
Robin Novak <robin.m.novak@gmail.com>
2020-10-01 15:12:23 -0700

    Fetch Post Comment

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -72,7 +72,7 @@ function CampsiteInfo(props) {
           <RenderCampsite campsite={props.campsite} />
           <RenderComments
             comments={props.comments}
-            addComment={props.addComment}
+            postComment={props.postComment}
             campsiteId={props.campsite.id}
           />
         </div>
@@ -83,7 +83,7 @@ function CampsiteInfo(props) {
   }
 }
 
-function RenderComments({ comments, addComment, campsiteId }) {
+function RenderComments({ comments, postComment, campsiteId }) {
   console.log(comments);
   if (comments) {
     return (
@@ -105,7 +105,7 @@ function RenderComments({ comments, addComment, campsiteId }) {
             ))}
           </p>
         </div>
-        <CommentForm campsiteId={campsiteId} addComment={addComment} />
+        <CommentForm campsiteId={campsiteId} postComment={postComment} />
       </>
     );
   }
@@ -130,7 +130,7 @@ class CommentForm extends Component {
 
   handleSubmit(values) {
     this.toggleModal();
-    this.props.addComment(
+    this.props.postComment(
       this.props.campsiteId,
       values.rating,
       values.author,
